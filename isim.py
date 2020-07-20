3afb3eccf894e82a355fb5f9c6b3527cff62fd6f
buraksmg <68508003+buraksmg@users.noreply.github.com>
2020-07-20 15:38:54 +0300

    Add files via upload

---
+++ isim.py
@@ -0,0 +1,15 @@
+"""isimli birşeyler"""
+isim=str(input("İsminizi giriniz: "))
+a=int(input('''Seçenekler:
+1. İsminiz büyük harfle yazılır. 
+2. İsminiz küçük harfle yazılır.
+3. İsminizdeki harfler büyükse küçük, küçükse büyük yazılır. '''))
+if a==1:
+    print(str.upper(isim))
+elif a==2:
+    print(str.lower(isim))
+elif a==3:
+    print(str.swapcase(isim))
+#elif a==4:
+    #b = 'Başgan'
+    #print(isim.join("Başgan"))
